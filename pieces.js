2125bf40ed8b287b44bea9b62d0149bc0d4f1fe4
posepastropdequestion <150798632+posepastropdequestion@users.noreply.github.com>
2023-11-20 11:19:24 +0000

    Réactivité des bouton de triage (affiche dans l'ordre dans la console).

--- pieces.js
+++ pieces.js
@@ -3,22 +3,15 @@ const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
 
-//const sectionFiches = document.querySelector(".fiches");
-//sectionFiches.appendChild(imageElement);
-//sectionFiches.appendChild(nomElement);
-//sectionFiches.appendChild(prixElement);
-//sectionFiches.appendChild(categorieElement);
-//sectionFiches.appendChild(descriptionElement);
-//sectionFiches.appendChild(disponibiliteElement);
 
 for (let i = 0; i < pieces.length; i++) {
 
 // Récupération de l'élément du DOM qui accueillera les fiches
-const sectionFiches = document.querySelector(".fiches");
+  const sectionFiches = document.querySelector(".fiches");
 // Création d’une balise dédiée à une pièce automobile
-const pieceElement = document.createElement("article");
+  const pieceElement = document.createElement("article");
 // On crée l’élément img.
-const imageElement = document.createElement("img");
+  const imageElement = document.createElement("img");
 // On accède à l’indice i de la liste pieces pour configurer la source de l’image.
   const article = pieces[i];
 
@@ -42,18 +35,80 @@ const imageElement = document.createElement("img");
                                 } 
       else{ (disponibiliteElement.innerText="Rupture de stock") }
 
-imageElement.src = pieces[i].image;
+  imageElement.src = pieces[i].image;
 // Idem pour le nom, le prix et la catégorie...
 
 // On rattache la balise article à la section Fiches
-sectionFiches.appendChild(pieceElement);
+  sectionFiches.appendChild(pieceElement);
 
 // On rattache l’image à pieceElement (la balise article)
-pieceElement.appendChild(imageElement);
-pieceElement.appendChild(nomElement);
-pieceElement.appendChild(prixElement);
-pieceElement.appendChild(categorieElement);
-pieceElement.appendChild(descriptionElement);
-pieceElement.appendChild(disponibiliteElement);
+  pieceElement.appendChild(imageElement);
+  pieceElement.appendChild(nomElement);
+  pieceElement.appendChild(prixElement);
+  pieceElement.appendChild(categorieElement);
+  pieceElement.appendChild(descriptionElement);
+  pieceElement.appendChild(disponibiliteElement);
 
 }
+
+
+const boutonTrier = document.querySelector(".btn-trier");
+boutonTrier.addEventListener("click", function () {
+    pieces.sort(function (a, b) {
+        return a.prix - b.prix;
+    });
+    console.log(pieces);
+});
+boutonTrier.addEventListener("click", function () {
+   const piecesOrdonnees = Array.from(pieces);
+   piecesOrdonnees.sort(function (a, b) {
+       return a.prix - b.prix;
+    });
+    console.log(piecesOrdonnees);
+});
+
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+
+boutonFiltrer.addEventListener("click", function () {
+   const piecesFiltrees = pieces.filter(function (piece) {
+       return piece.prix <= 35;
+   });
+  console.log(piecesFiltrees)
+});
+  boutonFiltrer.addEventListener("click", function () {
+   const piecesOrdonnees = Array.from(pieces);
+   piecesOrdonnees.sort(function (a, b) {
+       return a.prix - b.prix;
+    });
+    console.log(piecesOrdonnees);
+});
+const boutonDescription = document.querySelector(".btn-description");
+
+boutonDescription.addEventListener("click", function () {
+   const piecesDescription = pieces.filter(function (piece) {
+       return piece.description;
+   });
+  console.log(piecesDescription)
+});
+    boutonDescription.addEventListener("click", function () {
+   const piecesOrdonnees = Array.from(pieces);
+   piecesOrdonnees.sort(function (a, b) {
+       return a.prix - b.prix;
+    });
+    console.log(piecesOrdonnees);
+});
+
+const boutonDecroissant = document.querySelector(".btn-trier-decroissant");
+  boutonDecroissant.addEventListener("click", function () {
+    pieces.sort(function (a, b) {
+        return b.prix - a.prix;
+    });
+    console.log(pieces);
+});
+  boutonDecroissant.addEventListener("click", function () {
+   const piecesOrdonnees = Array.from(pieces);
+   piecesOrdonnees.sort(function (a, b) {
+       return a.prix - b.prix;
+    });
+    console.log(piecesOrdonnees);
+});
